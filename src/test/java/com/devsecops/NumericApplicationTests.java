d51b65c57c42c7a81806d1e364f7049d1a31ade5
user20201901 <66615228+user20201901@users.noreply.github.com>
2023-11-30 15:47:09 +0100

    Update NumericApplicationTests.java

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -49,7 +49,7 @@ public class NumericApplicationTests {
     @Test
     public void welcomeMessage() throws Exception {
                  this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("Kubernetes DevSecOps"))
+                .andExpect(content().string("Kubernetes DevSecOps"));
     }
     
 
